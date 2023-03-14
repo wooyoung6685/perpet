7d6755f663cc90ed23a2c6df951f1020e0b6110d
wooyoung6685 <wooyoung6685@gmail.com>
2023-03-14 17:05:42 +0900

    cwy_header

---
+++ src/components/Header/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="logo">
+      <a href="/">
+        <img src="./images/Header/logo.svg" />
+      </a>
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/components/Header/NavButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NavButton.css";
+
+function NavButton() {
+  return (
+    <div>
+      <a href="/" className="navbutton_img">
+        <img src="./images/Header/gnb_banner_new.svg" />
+      </a>
+    </div>
+  );
+}
+
+export default NavButton;
