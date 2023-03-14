16778fb1bce2bceb2fd9e1229222f47b77e35d2a
parkcheonju <cjswne@gmail.com>
2023-03-15 00:04:53 +0900

    pcj- reset_css add and brand,footer css modify commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import "./Footer.css";
 function Footer() {
   return (
     <>
-      <div className="footer_inner mg_top-20">
-        <div className="flex">
+      <div className="footer_inner ">
+        <div className="flex mg_top-20">
           <img src="./images/Footer/logo.svg" alt="myImage" />
         </div>
         <div className="flex mg_top-20">
@@ -20,19 +20,19 @@ function Footer() {
           <div className="mg_left-10">사업자등록번호 12-34-5678</div>
           <div className="mg_left-10">주소 서울특별시 강남구 가나다 12, 마바마바 아자아자</div>
         </div>
-        <div className="flex mg_top-10">
+        <div className="flex mg_top-20">
           <div>통신 판매업 제 1234- 1234</div>
           <div className="mg_left-10">개인정보보호책임자 : 김춘복</div>
           <div className="mg_left-10">팩스 12-345-6789</div>
         </div>
-        <div className="flex mg_top-10">
+        <div className="flex mg_top-20">
           <div className="footer_font-small">Tel: 00-1234-5678 </div>
           <div className="footer_font-small mg_left-10">Email : helloanimal@test.com</div>
-        </div>
-        <div className="icon">
-          <div>
-            <img src="./images/Footer/facebook.png" alt="facebook" />
-            <img className="mg_left-10" src="./images/Footer/instagram.png" alt="instagram" />
+          <div className="icon">
+            <div>
+              <img src="./images/Footer/facebook.png" alt="facebook" />
+              <img className="mg_left-10" src="./images/Footer/instagram.png" alt="instagram" />
+            </div>
           </div>
         </div>
       </div>
