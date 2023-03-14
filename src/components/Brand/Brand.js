635c03001b1107414ce505b8032e5fc442696f73
parkcheonju <cjswne@gmail.com>
2023-03-14 18:25:02 +0900

    pcj-Brand Footer commit

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -1,7 +1,80 @@
 import React from "react";
 import "./Brand.css";
+
 function Brand() {
-  return <div>Brand</div>;
+  const images = [
+    {
+      id: 1,
+      src: "./images/Brand/orizen.png",
+      brandname: "오리젠",
+    },
+    {
+      id: 2,
+      src: "./images/Brand/acana.png",
+      brandname: "아카나",
+    },
+    {
+      id: 3,
+      src: "./images/Brand/royalcanin.png",
+      brandname: "로얄캐닌",
+    },
+    {
+      id: 4,
+      src:"./images/Brand/attu.jpg",
+      brandname: "아투",
+    },
+    {
+      id: 5,
+      src: "./images/Brand/now.png",
+      brandname: "NOW",
+    },
+    {
+      id: 6,
+      src: "./images/Brand/hills.png",
+      brandname: "힐스",
+    },
+    {
+      id: 7,
+      src: "./images/Brand/ziwipeak.png",
+      brandname: "지위픽",
+    },
+    {
+      id: 8,
+      src: "./images/Brand/proplan.png",
+      brandname: "프로플랜",
+    },
+    {
+      id: 9,
+      src: "./images/Brand/rawz.png",
+      brandname: "로우즈",
+    },
+    {
+      id: 10,
+      src: "./images/Brand/cesar.png",
+      brandname: "시저",
+    }
+  ];
+
+  return (
+    <>
+      <div className="brand_inner brand_mg-top">
+        <div className="brand_flex">
+          <p className="brand_text-01">국내 최대 브랜드 보유!</p>
+          <p className="brand_text-02">더보기</p>
+        </div>
+        <div className="brand_name">
+          {
+          images.map((image) => (
+            <div className='brand_card-box brand_mg-top' key={image.id}>
+              <img src={image.src} alt={image.brandname} />
+              <p className='brand_text-03'>{image.brandname}</p>
+            </div>
+          ))
+          }
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Brand;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,42 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="footer_inner mg_top-20">
+        <div className="flex">
+          <img src="./images/Footer/logo.svg" alt="myImage" />
+        </div>
+        <div className="flex mg_top-20">
+          <div>회사소개</div>
+          <div className="mg_left-10 footer_font-bold">개인정보처리방침</div>
+          <div className="mg_left-10">이용약관</div>
+          <div className="mg_left-10">제휴입점 및 문의</div>
+          <div className="mg_left-10">사용자 정보</div>
+        </div>
+        <div className="flex mg_top-20">
+          <div>대표자 : 박규복 </div>
+          <div className="mg_left-10">사업자등록번호 12-34-5678</div>
+          <div className="mg_left-10">주소 서울특별시 강남구 가나다 12, 마바마바 아자아자</div>
+        </div>
+        <div className="flex mg_top-10">
+          <div>통신 판매업 제 1234- 1234</div>
+          <div className="mg_left-10">개인정보보호책임자 : 김춘복</div>
+          <div className="mg_left-10">팩스 12-345-6789</div>
+        </div>
+        <div className="flex mg_top-10">
+          <div className="footer_font-small">Tel: 00-1234-5678 </div>
+          <div className="footer_font-small mg_left-10">Email : helloanimal@test.com</div>
+        </div>
+        <div className="icon">
+          <div>
+            <img src="./images/Footer/facebook.png" alt="facebook" />
+            <img className="mg_left-10" src="./images/Footer/instagram.png" alt="instagram" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
