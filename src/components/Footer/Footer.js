a2d38a9c40289851a052743c8fc80f1e79c975bb
parkcheonju <cjswne@gmail.com>
2023-03-15 18:07:01 +0900

    pcj- footer css modify commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,40 +1,67 @@
 import React from "react";
 import "./Footer.css";
-
 function Footer() {
   return (
     <>
       <div className="footer_inner ">
-        <div className="flex footer_pd-left footer_mg-top60">
+        <div className="flex footer_pd-left footer_mg-top-60">
           <img src="./images/Footer/logo.svg" alt="myImage" />
         </div>
-        <div className='footer_pd-left test_font'>
-          <div className="flex footer_mg-top-20 ">
-            <div>회사소개</div>
-            <div className="footer_mg-left-10 footer_font-bold ">개인정보처리방침</div>
-            <div className="footer_mg-left-10">이용약관</div>
-            <div className="footer_mg-left-10">제휴입점 및 문의</div>
-            <div className="footer_mg-left-10">사용자 정보</div>
+        <div className="footer_pd-left footer_font">
+          <div className="flex footer_mg-top-20">
+            <div className="footer_font-top">
+              <p className="footer_font-style">회사소개</p>
+            </div>
+            <div className="footer_font-top2 footer_font-bold">
+              <p className="footer_font-style">개인정보처리방침</p>
+            </div>
+            <div className="footer_font-top2 footer_color">
+              <p className="footer_font-style">이용약관</p>
+            </div>
+            <div className="footer_font-top2 footer_color">
+              <p className="footer_font-style">제휴입점 및 문의</p>
+            </div>
+            <div className="footer_font-top2 footer_color">사용자 정보</div>
           </div>
           <div className="flex footer_mg-top-20 ">
-            <div>대표자 : 박규복 </div>
-            <div className="footer_mg-left-10">사업자등록번호 12-34-5678</div>
-            <div className="footer_mg-left-10">주소 서울특별시 강남구 가나다 12, 마바마바 아자아자</div>
+            <div className="footer_mg-right-20">
+              <span className="footer_font-1">대표자</span>
+              <span className="footer_font-2 ">박규복</span>
+            </div>
+            <div className="footer_margin-01">
+              <span className="footer_font-1">사업자등록번호</span>
+              <span className="footer_font-2">12-34-5678</span>
+            </div>
+            <div className="footer_margin-01">
+              <span className="footer_font-1">주소</span>
+              <span className="footer_font-3">서울특별시 강남구 가나다 12, 마바마바 아자아자</span>
+            </div>
           </div>
-          <div className="flex footer_mg-top-20 ">
-            <div>통신 판매업 제 1234- 1234</div>
-            <div className="footer_mg-left-10">개인정보보호책임자 : 김춘복</div>
-            <div className="footer_mg-left-10">팩스 12-345-6789</div>
+          <div className="flex footer_mg-top-20">
+            <div className="footer_mg-right-30">
+              <span className="footer_font-1">통신 판매업</span>
+              <span className="footer_font-2">제 1234- 1234</span>
+            </div>
+            <div className="footer_mg-left-10 footer_mg-right-30">
+              <span className="footer_font-1">개인정보보호책임자</span>
+              <span className="footer_font-2">김춘복</span>
+            </div>
+            <div className="footer_mg-left-10">
+              <span className="footer_font-1">팩스</span>
+              <span className="footer_font-3">12-345-6789</span>
+            </div>
           </div>
-          <div className="flex footer_mg-top-20 ">
-            <div className="footer_font-small test_font2">Tel: 00-1234-5678 </div>
-            <div className="footer_font-small footer_mg-left-10 test_font2">Email : helloanimal@test.com</div>
+          <div className="flex footer_mg-top-10 ">
+            <div className="footer_font3">Tel: 00-1234-5678 </div>
+            <div className="footer_mg-left-10 footer_font3">Email : helloanimal@test.com</div>
             <div className="icon">
-              <div className='footer_pd-right'>
+              <div className="footer_pd-right">
                 <img src="./images/Footer/facebook.png" alt="facebook" />
                 <img className="footer_mg-left-10" src="./images/Footer/instagram.png" alt="instagram" />
               </div>
             </div>
+          </div>
+          <div className='footer_mg-top-60'>
           </div>
         </div>
       </div>
