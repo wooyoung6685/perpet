cc1eaf9720bfbd155037ac2cf89b14480afbbd24
parkcheonju <cjswne@gmail.com>
2023-03-16 18:12:40 +0900

    pcj- App.js modify commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import Banner from "./components/Banner/Banner";
 import IconSlider from "./components/IconSlider/IconSlider";
 import Product from "./components/Product/Product";
 import Brand from "./components/Brand/Brand";
-
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter } from "react-router-dom";
 import MultipleSlide from "./components/Multipleslide/MultipleSlide";
-import { useState } from "react";
 
 function App() {
   return (
