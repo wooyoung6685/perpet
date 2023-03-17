83e3d684138851b36ece3860ef350dae88feb893
M9 <9yu6820@gmail.com>
2023-03-17 17:29:39 +0900

    kmg-hotdeal array mao fix

--- src/components/Product/ProductItem.js
+++ src/components/Product/ProductItem.js
@@ -1,33 +1,33 @@
 import React from "react";
 import "./ProductItem.css";
-import { Rate } from 'antd';
+import { Rate } from "antd";
 import { Link } from "react-router-dom";
 
-function ProductItem({ name, price, seller, img, orgPrice, disc }) {
+function ProductItem({ name, price, seller, image, orgPrice, disc }) {
   return (
-    <div id="product-list">
-      <div className="product-card">
-        <Link to="/">
-          <div className="product-img_box">
-            <img className="product-img" src={img} alt="product-img" />
+    <div id='product-list'>
+      <div className='product-card'>
+        <Link to='/'>
+          <div className='product-img_box'>
+            <img className='product-img' src={image} alt='product-img' />
           </div>
         </Link>
-        <div className="product-content">
-          <span className="product-seller">{seller}</span>
-          <span className="product-name">{name}</span>
-          <span className="price-box">
-            <div className="origin-price">{orgPrice}</div>
-            <span className="product-disc">{disc}</span>
-            <span className="product-price">{price}</span>
+        <div className='product-content'>
+          <span className='product-seller'>{seller}</span>
+          <span className='product-name'>{name}</span>
+          <span className='price-box'>
+            <div className='origin-price'>{orgPrice}</div>
+            <span className='product-disc'>{disc}</span>
+            <span className='product-price'>{price}</span>
           </span>
           <span>
-            <Rate className="star" disabled defaultValue={3} />
-            <span className="star_num"> 3</span>
+            <Rate className='star' disabled defaultValue={3} />
+            <span className='star_num'> 3</span>
           </span>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ProductItem;
--- src/components/Product/Product_sec1.js
+++ src/components/Product/Product_sec1.js
@@ -9,7 +9,7 @@ import { Navigation } from "swiper";
 function Product_sec1() {
   const hotdeal = [
     {
-      image: "./images/Product/pet_acc1.png",
+      image: "images/Product/pet_acc1.png",
       name: "하네스",
       price: "18,000원",
       orgPrice: "38,000원",
@@ -17,7 +17,7 @@ function Product_sec1() {
       seller: "펫베오",
     },
     {
-      image: "./images/Product/pet_food1.jpg",
+      image: "images/Product/pet_food1.jpg",
       name: "양치간식",
       price: "24,900원",
       orgPrice: "49,900원",
@@ -25,7 +25,7 @@ function Product_sec1() {
       seller: "그리니스",
     },
     {
-      image: "./images/Product/pet_toy1.jpg",
+      image: "images/Product/pet_toy1.jpg",
       name: "말장난감",
       price: "9,000원",
       orgPrice: "5,300원",
@@ -33,7 +33,7 @@ function Product_sec1() {
       seller: "말",
     },
     {
-      image: "./images/Product/pet_house1.jpg",
+      image: "images/Product/pet_house1.jpg",
       name: "집",
       orgPrice: "40,000원",
       price: "32,000원",
@@ -41,75 +41,43 @@ function Product_sec1() {
       seller: "아늑해",
     },
     {
-      image: "",
-      name: "",
-      price: "",
-      orgPrice: "",
-      discount: "",
-      seller: "",
+      image: "images/Product/pet_food2.jpg",
+      name: "사료",
+      orgPrice: "25,000원",
+      price: "25,000원",
+      discount: "50%",
+      seller: "네추럴코어",
     },
     {
-      image: "",
-      name: "",
-      price: "",
-      orgPrice: "",
-      discount: "",
-      seller: "",
+      image: "images/Product/pet_toy2.jpg",
+      name: "당근노즈워크",
+      orgPrice: "12,500원",
+      price: "10,000원",
+      discount: "50%",
+      seller: "노즈워크",
     },
     {
-      image: "",
-      name: "",
-      price: "",
-      orgPrice: "",
-      discount: "",
-      seller: "",
+      image: "images/Product/pet_acc2.jpg",
+      name: "넥카라",
+      orgPrice: "3,500원",
+      price: "7,000원",
+      discount: "50%",
+      seller: "핥지마",
     },
     {
-      image: "",
-      name: "",
-      price: "",
-      orgPrice: "",
-      discount: "",
-      seller: "",
-    },
-    {
-      image: "",
-      name: "",
-      price: "",
-      orgPrice: "",
-      discount: "",
-      seller: "",
-    },
-    {
-      image: "",
-      name: "",
-      price: "",
-      orgPrice: "",
-      discount: "",
-      seller: "",
-    },
-    {
-      image: "",
-      name: "",
-      price: "",
-      orgPrice: "",
-      discount: "",
-      seller: "",
-    },
-    {
-      image: "",
-      name: "",
-      price: "",
-      orgPrice: "",
-      discount: "",
-      seller: "",
+      image: "images/Product/pet_house2.jpg",
+      name: "집",
+      orgPrice: "20,000원",
+      price: "40,000원",
+      discount: "50%",
+      seller: "아늑해",
     },
   ];
   return (
     <div>
       <h2 className='product-title'>핫딜상품🔥</h2>
       <Swiper
-        slidesPerView={1}
+        slidesPerView={4}
         spaceBetween={30}
         loop={true}
         pagination={{
@@ -119,46 +87,17 @@ function Product_sec1() {
         modules={[Navigation]}
         className='Product_slide'
       >
-        <SwiperSlide>
-          <div id='product-list'>
-            <div className='hotdeal'>
-              <ProductItem image='./images/Product/pet_acc1.png' name='하네스' orgPrice='38,000원' discount='53% ' price='18,000원' seller='펫베오' />
-            </div>
-            <div className='hotdeal'>
-              <ProductItem image='./images/Product/pet_food1.jpg' name='양치간식' orgPrice='49,900원' discount='50% ' price='24,900원' seller='그리니스' />
-            </div>
-            <div className='hotdeal'>
-              <ProductItem image='./images/Product/pet_toy1.jpg' name='말장난감' orgPrice='9,000원' discount='41% ' price='5,300원' seller='말' />
-            </div>
-            <div className='hotdeal'>
-              <ProductItem image='./images/Product/pet_house1.jpg' name='집' orgPrice='40,000원' discount='20% ' price='32,000원' seller='아늑해' />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id='product-list'>
-            <ProductItem image='./images/Product/pet_food2.jpg' name='사료' orgPrice='25,000원' price='25,000원' discount='50%' seller='네추럴코어' />
-            <ProductItem image='./images/Product/pet_toy2.jpg' name='당근노즈워크' orgPrice='12,500원' price='10,000원' discount='50%' seller='노즈워크' />
-            <ProductItem image='./images/Product/pet_acc2.jpg' name='넥카라' orgPrice='3,500원' price='7,000원' discount='50%' seller='핥지마' />
-            <ProductItem image='./images/Product/pet_house2.jpg' name='집' orgPrice='20,000원' price='40,000원' discount='50%' seller='아늑해' />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id='product-list'>
-            <ProductItem image='./images/Product/pet_toy3.jpg' name='바나나장난감' price='10,000원' seller='바나나' />
-            <ProductItem image='./images/Product/pet_food3.jpg' name='개껌' price='25,000원' seller='네추럴코어' />
-            <ProductItem image='./images/Product/pet_house3.jpg' name='집' price='40,000원' seller='아늑해' />
-            <ProductItem image='./images/Product/pet_acc3.png' name='목줄' price='7,000원' seller='안전하개' />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id='product-list'>
-            <ProductItem image='./images/Product/pet_food4.jpg' name='양치간식' price='25,000원' seller='덴티페어리' />
-            <ProductItem image='./images/Product/pet_toy4.png' name='장난감' price='10,000원' seller='그루비볼' />
-            <ProductItem image='./images/Product/pet_acc4.jpg' name='하네스' price='7,000원' seller='산책좋아' />
-            <ProductItem image='./images/Product/pet_house4.jpg' name='집' price='40,000원' seller='포근해' />
-          </div>
-        </SwiperSlide>
+        {hotdeal.map((hotdealItem, idx) => {
+          return (
+            <SwiperSlide key={idx}>
+              <div id='product-list'>
+                <div className='hotdeal'>
+                  <ProductItem image={`${hotdealItem.image}`} name={hotdealItem.name} orgPrice={hotdealItem.orgPrice} discount={hotdealItem.discount} price={hotdealItem.price} seller={hotdealItem.seller} />
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
