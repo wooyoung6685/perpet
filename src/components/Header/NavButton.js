81dcc1fb6b3f0f74a761e77806f4527fd7a81b08
wooyoung6685 <wooyoung6685@gmail.com>
2023-03-23 17:18:06 +0900

    cwy_navbutton

--- src/components/Header/NavButton.js
+++ src/components/Header/NavButton.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./NavButton.css";
+import { Modal } from "antd";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function NavButton() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div>
-      <a href="/" className="navbutton_img">
+    <div className="navButton">
+      <Link onClick={showModal}>
         <img src="./images/Header/gnb_banner_new.svg" />
-      </a>
+      </Link>
+      <Modal title="" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <img src="./images/EventBanner/ExpirationDateGuaranteePc.jpg" />
+      </Modal>
     </div>
   );
 }
--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Main from "../../Main";
 import LoginPage from "../Member/LoginPage";
 import Membership from "../Member/Membership";
