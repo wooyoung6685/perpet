cc6de418b9fc4e6a94fc84e0735500eeba4e93d5
M9 <9yu6820@gmail.com>
2023-03-28 12:16:50 +0900

    kmg- merge error fixed

--- src/components/Product/ProductSec2.js
+++ src/components/Product/ProductSec2.js
@@ -29,8 +29,8 @@ function ProductSec2() {
 
   return (
     <div>
-      <h2 className="product-title">이 상품 어때❔</h2>
-      <div class="ParentProduct2">
+      <h2 className='product-title'>이 상품 어때❔</h2>
+      <div class='ParentProduct2'>
         <Swiper
           slidesPerView={1}
           spaceBetween={10}
@@ -40,7 +40,7 @@ function ProductSec2() {
           }}
           navigation={true}
           modules={[Navigation]}
-          className="Product_slide1"
+          className='Product_slide1'
           breakpoints={{
             640: {
               slidesPerView: 2,
@@ -60,13 +60,8 @@ function ProductSec2() {
             if (!product1.discount) {
               return (
                 <SwiperSlide key={idx}>
-<<<<<<< HEAD
-                  <div id="product-list">
-                    <ProductItem id={product1.id} image={`${API_URL}/${product1.image}`} name={product1.name} price={product1.orgPrice} seller={product1.seller} />
-=======
                   <div id='product-list'>
                     <ProductItem id={product1.id} image={`${API_URL}/${product1.image}`} name={product1.name} price={product1.orgPrice} seller={product1.seller} soldout={product1.soldout} />
->>>>>>> 4498a93f81d8449840ff0aefd3a2c96901c87f09
                   </div>
                 </SwiperSlide>
               );
