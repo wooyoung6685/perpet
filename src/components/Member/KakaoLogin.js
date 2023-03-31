ea2964f7c678f3f4fa9ad6cd173db2a52dd62033
parkcheonju <cjswne@gmail.com>
2023-03-31 17:48:41 +0900

    pcj-KakaoLogin modify commit

---
+++ src/components/Member/KakaoLogin.js
@@ -0,0 +1,13 @@
+function KakaoLogin() {
+  const REST_API_KEY = "52d0be4ae43601c439b012f8889eebf7";
+  const REDIRECT_URI = "http://localhost:3000";
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const kakaologin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  }
+  return {
+    kakaologin
+  };
+}
+
+export default KakaoLogin;
