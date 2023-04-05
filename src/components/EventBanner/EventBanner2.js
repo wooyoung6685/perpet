51304e661b35f973cd2ebadffd9525521dd1bc34
parkcheonju <cjswne@gmail.com>
2023-04-05 14:37:49 +0900

    pcj- css modify commit

--- src/components/EventBanner/EventBanner2.js
+++ src/components/EventBanner/EventBanner2.js
@@ -4,10 +4,12 @@ import "./EventBanner2.css";
 
 function EventBanner2({ img, category }) {
   return (
-    <div className='eventBanner2'>
-      <Link to={`products/category/${category}`}>
-        <img src={img} alt={img}/>
-      </Link>
+    <div className='eventBanner2_mg'>
+      <div className='eventBanner2'>
+        <Link to={`products/category/${category}`}>
+          <img src={img} alt={img}/>
+        </Link>
+      </div>
     </div>
   );
 }
